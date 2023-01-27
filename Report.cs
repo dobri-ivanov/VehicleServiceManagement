049deaa0aac3afb62d78b10b93212242b8604f79
dobri-ivanov <dobri0316@abv.bg>
2023-01-27 15:35:56 +0200

    Update

---
+++ Report.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleServiceManagement
+{
+    class Report
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int ClientID { get; set; }
+        public int VehicleID{ get; set; }
+        public decimal WorkPrice { get; set; }
+        public decimal TotalSum { get; set; }
+
+    }
+}
