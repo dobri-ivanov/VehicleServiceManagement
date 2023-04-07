f0dcc9723c5f776d5bf4ec24eae2889641001982
dobri-ivanov <dobri0316@abv.bg>
2023-04-07 13:11:29 +0300

    Edited files

--- Main/Main.cs
+++ Main/Main.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VehicleServiceManagement
@@ -830,6 +823,11 @@ namespace VehicleServiceManagement
         private void ImageButtonRefreshReports_Click(object sender, EventArgs e)
         {
             FillRaportsTable();
+        }
+
+        private void TextBoxSearchReport_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
